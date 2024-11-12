b63d3e74197b44bf2d4a205cf4fc99a8734753fd
Katy Welyczko <katy.jane8@gmail.com>
2024-11-12 12:57:25 -0700

    change font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,9 @@
-import localFont from "next/font/local";
 import "./globals.css";
+import { Roboto } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
 });
 
 export default function RootLayout({
@@ -17,9 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${roboto} antialiased`}>
         {children}
       </body>
     </html>
